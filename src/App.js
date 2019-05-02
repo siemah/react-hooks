5e81bdd2d3635bfe443641c811c3c676690b2a98
siemah <m_saidouni@inptic.edu.dz>
2019-05-02 15:02:38 +0200

    (Build)useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,56 +1,23 @@
-import React from 'react';
-import './App.css';
-import Button from './components/Button'
-import { ThemeContext, themes } from './context/theme'
-import { UserContext, } from './context/user'
+import React, { useState, useEffect } from 'react';
+import './App.css'
 
-const Block = (props) => {
+const App = props => {
+  
+  const [ count, setCount] = useState(0);
+  const [ name, setName ] = useState('guest');
+  const updateTitle = () => {
+    console.log("effect of updating a title", count);
+    document.title = `You clicked ${count} time :)`;
+  };
+  useEffect(updateTitle);
+
   return (
-    <div className='block'>
-      <Button />
+    <div className="App" style={{height: '200vh'}}>
+      <p>{name} clicked {count} time :)</p>
+      <button onClick={() => setCount(count+1)}>click me</button>
+      <input onChange={({target}) => setName(target.value)} />
     </div>
   )
 }
-class App extends React.Component {
- 
-  constructor(props) {
-    super(props);
-    const toggleTheme = () => {
-      this.setState(prevS => ({
-        theme:
-          prevS.theme === themes.dark
-            ? themes.light
-            : themes.dark
-      }))
-    }
-    this.state = {
-      theme: themes.light,
-      toggleTheme,
-      user: {
-        name: "siemah"
-      }
-    }
-    
-  }
-
-  changeName = ({ target }) => this.setState({user: {name: target.value}})
-
-  render (){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <ThemeContext.Provider value={this.state}>
-            <UserContext.Provider value={this.state.user}>
-              <Block />
-            </UserContext.Provider>
-          </ThemeContext.Provider>
-          <input placeholder='change the name' onChange={this.changeName} />
-        </header>
-      </div>
-    );
-  }
-}
-
-App.contextType = ThemeContext;
 
 export default App;
